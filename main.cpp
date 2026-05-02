2c52d4ed444b29fddb1357e544a291899c340957
Dominyka <dominyka.juchneviciute@gmail.com>
2026-05-02 18:04:39 +0300

    Added ClearPixels method

--- main.cpp
+++ main.cpp
@@ -31,7 +31,16 @@ public:
     {
         return height;
     }
-    clearPixel(color)
+    void PixelBuffer::clear(uint8_t r, uint8_t g, uint8_t b, uint8_t a)
+    {
+        for (int i = 0; i < width * height; ++i)
+        {
+            pixels[i * 4 + 0] = r;
+            pixels[i * 4 + 1] = g;
+            pixels[i * 4 + 2] = b;
+            pixels[i * 4 + 3] = a;
+        }
+    }
 };
 
 
