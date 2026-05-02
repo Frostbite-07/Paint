21b33e58b30716fe22708b3248b1424845b9b64e
Matas Čekanauskas <matascekas@gmail.com>
2026-05-02 16:52:30 +0300

    Defined methods and fields

--- Paint/Display/TextureManager.cpp
+++ Paint/Display/TextureManager.cpp
@@ -5,8 +5,9 @@
 #include "TextureManager.h"
 #include <SFML/Graphics.hpp>
 
-class TextureManager {
-private:
-    texture::Texture texture;
+    void update(std::vector<uint8_t>, PixelBuffer&) {
 
-};
+    }
+    sf::Texture& TextureManager::getTexture(sf::Texture& texture) const {
+        return texture;
+    }
--- Paint/Display/TextureManager.h
+++ Paint/Display/TextureManager.h
@@ -4,7 +4,18 @@
 
 #ifndef TP_TEXTUREMANAGER_H
 #define TP_TEXTUREMANAGER_H
+#include <vector>
+#include <SFML/Graphics/Texture.hpp>
 
+class TextureManager {
+    private:
+    sf::Texture texture;
+    bool needsUpdate;
 
+    public:
+    void update(std::vector<uint8_t> pixels, PixelBuffer buffer);
+    sf::Texture& getTexture(sf::Texture& texture) const;
+
+};
 
 #endif //TP_TEXTUREMANAGER_H
