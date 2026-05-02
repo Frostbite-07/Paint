4e368482c8db717e3ac687c23fd8a813bda5105e
Matas Čekanauskas <matascekas@gmail.com>
2026-05-02 16:08:26 +0300

    Reorganized folder structure and created the file for TextureManager

---
+++ Paint/Display/TextureManager.cpp
@@ -0,0 +1,12 @@
+//
+// Created by frostbite on 2026-05-02.
+//
+
+#include "TextureManager.h"
+#include <SFML/Graphics.hpp>
+
+class TextureManager {
+private:
+    texture::Texture texture;
+
+};
---
+++ Paint/Display/TextureManager.h
@@ -0,0 +1,10 @@
+//
+// Created by frostbite on 2026-05-02.
+//
+
+#ifndef TP_TEXTUREMANAGER_H
+#define TP_TEXTUREMANAGER_H
+
+
+
+#endif //TP_TEXTUREMANAGER_H
